7f54890e73608fe5cf9c5f57b544169ad02fdd4e
JulianOviedo <julianovie234@gmail.com>
2023-02-10 21:39:49 -0300

    add useSearch for validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import './App.css'
 import Movies from './components/Movies/Movies'
 import { useMovies } from './hooks/useMovies'
+import useSearch from './hooks/useSearch'
 
 function App() {
-  const handleSubmit = () => {
+
+  const { movies: mappedMovies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  const handleSubmit = (e) => {
     e.preventDefault()
   }
 
-  const { movies: mappedMovies } = useMovies()
+  const handleChange = (e) => {
+    const newSearch = e.target.value
+    if (newSearch.startsWith(' ')) return
+    updateSearch(newSearch)
+  }
 
   return (
     <>
       <header>
         <h1>Movie Browser</h1>
         <form className="form">
-          <input type='text' placeholder="Avengers, The Matrix, Lord of the Rings ....."></input>
+          <input onChange={handleChange}
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }}
+            value={search}
+            type='text'
+            placeholder="Avengers, The Matrix, Lord of the Rings .....">
+          </input>
           <button onClick={handleSubmit} type="submit">Search</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
