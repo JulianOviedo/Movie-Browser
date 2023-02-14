c4751344ec53b0061354c59b039f610d85113b97
JulianOviedo <julianovie234@gmail.com>
2023-02-14 11:42:39 -0300

    add searchMovies hook

---
+++ src/services/searchMovies.js
@@ -0,0 +1,25 @@
+const API_KEY = '4287ad07'
+
+export default async function searchMovies ({search}) {
+    if (search === '') return null
+    
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        console.log(response)
+        const json = await response.json()
+
+        const movies = json.Search
+        return movies?.map(movies => (
+            {
+                id: movies.imdbID,
+                title: movies.Title,
+                year: movies.Year,
+                poster: movies.Poster
+            }
+        ))
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error('Error searching movies.')
+    }
+}
